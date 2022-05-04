69144972df918a4fa841a7e24a69a8fe66b73c9d
stocksociety <arnold.kagunda@student.moringaschool.com>
2022-05-04 08:10:24 +0300

    remove heroku

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,16 +11,10 @@ import static spark.Spark.*;
 
 
 public class App {
-    static int getHerokuAssignedPort() {
-        ProcessBuilder processBuilder = new ProcessBuilder();
-        if (processBuilder.environment().get("PORT") != null) {
-            return Integer.parseInt(processBuilder.environment().get("PORT"));
-        }
-        return 4567;
-    }
+
 
     public static void main(String[] args) {
-        port(getHerokuAssignedPort());
+  
         staticFileLocation("/public");
         //Root Route
         get("/", (request, response) -> {
