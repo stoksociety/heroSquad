1685f4fe40cbeb5f74c6563863d12e0547c788ec
stocksociety <arnold.kagunda@student.moringaschool.com>
2022-05-04 08:29:37 +0300

    resolve

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -16,7 +16,7 @@ public class App {
         if (processBuilder.environment().get("PORT") != null) {
             return Integer.parseInt(processBuilder.environment().get("PORT"));
         }
-     
+        return 4567;
     }
 
     public static void main(String[] args) {
